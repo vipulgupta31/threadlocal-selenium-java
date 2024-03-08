a29a148dfd3d4038692f88605bd32d87e1d00535
Vipul Gupta <71564722+vipulgupta31@users.noreply.github.com>
2024-03-08 13:45:36 +0530

    Add files via upload

---
+++ CloudGrid/src/test/java/WithThreadLocal/advanced-scenarios/TestOrderFlowEcommercePlayground.java
@@ -0,0 +1,26 @@
+package WithThreadLocal.advanced-scenarios;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import WithThreadLocal.BaseTest;
+
+public class TestOrderFlowEcommercePlayground extends BaseTest
+{
+	@Test
+	public void testOrderFlow() 
+	{
+		System.out.println("Thread id " + Thread.currentThread().getId() + " : Navigating to LambdaTest Ecommerce website");
+		getDriver().get("https://ecommerce-playground.lambdatest.io/");
+		
+		System.out.println("Thread id " + Thread.currentThread().getId() + " : Entering search data");
+		getDriver().findElement(By.name("search")).sendKeys("iphone");
+		
+		System.out.println("Thread id " + Thread.currentThread().getId() + " : Clicking search button");
+		getDriver().findElement(By.xpath("//*[text()='Search']")).click();
+		
+		System.out.println("Thread id " + Thread.currentThread().getId() + " : Fetch the price of first item and print it");
+		String price = getDriver().findElement(By.xpath("(//*[@class='price-new'])[1]")).getText();
+		System.out.println("Thread id " + Thread.currentThread().getId() + " : Final message = " + price);
+	}	
+}
